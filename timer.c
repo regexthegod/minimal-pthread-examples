173dde5206c95c451f4692372e24ea572598fd58
Reggie Ecks <56512282+regexthegod@users.noreply.github.com>
2019-10-13 14:00:11 -0400

    Update timer.c
    Thanks Rasbora

--- timer.c
+++ timer.c
@@ -5,39 +5,30 @@
 #include <pthread.h>
 
 void* function() { // function that threads will be calling
-
-	printf("Thread spawned\n");
-
-	while(1){} // give the function something to do
+    printf("Thread spawned\n");
+    while(1){} // give the function something to do
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-	if(argc < 2){
+    if(argc < 3) {
+        fprintf(stderr, "Usage: %s <threads> <time>\n", argv[0]);
+        exit(1);
+    }
 
-	fprintf(stderr, "Usage: %s <threads> <time>\n", argv[0]);
-	exit(1);
-	}
+    pthread_t thread_id;
 
-	pthread_t thread_id;
+    for(int i = 0; i < atoi(argv[1]); i++) { // spawn number of threads based on argv[1]
+        pthread_create(&thread_id, NULL, &function, NULL);
+    }
 
-	for(int i = 0; i < atoi(argv[1]); i++) { // spawn number of threads based on argv[1]
+    int seconds = 0; // exit cleanly after number of seconds based on argv[2]
 
-	pthread_create(&thread_id, NULL, &function, NULL);
-	}
+    while(1) {
+        sleep(1);
+        seconds++;
+        if(seconds >= atoi(argv[2])) exit(1);
+    }
 
-	int seconds = 0; // exit cleanly after number of seconds based on argv[2]
-
-	while(1){
-	seconds++;
-      
-		if(seconds >= atoi(argv[2])) {
-			
-		exit(1);
-		}
-		
-		sleep(1);
-	}
-
-return 0;
+    return 0;
 }
