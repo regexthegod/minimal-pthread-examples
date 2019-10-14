e574c3ee01ed9ddde6d00e45da9cbfc42f1ff0bc
Reggie Ecks <56512282+regexthegod@users.noreply.github.com>
2019-10-13 22:17:26 -0400

    Update arguments.c

--- arguments.c
+++ arguments.c
@@ -4,46 +4,26 @@
 
 #include <pthread.h>
 
-struct arg_struct { // declare structure containing our arguments
+struct arg_struct {
 	int arg1;
 	int arg2;
 	int arg3;
 	int arg4;
 };
 
-void* function(void *arguments) { // function that threads will be calling
+void* function(void *arguments) {
 	struct arg_struct *args = arguments; 
-	
-	printf("Thread spawned with arguments: %i, %i, %i, %i\n", args->arg1, args->arg2, args->arg3, args->arg4);
+	printf("Function thread spawned with arguments: %i, %i, %i, %i\n", args->arg1, args->arg2, args->arg3, args->arg4);
 	return NULL;
 }
 
-int main(int argc, char *argv[]){
-
-	if(argc < 2){
-		fprintf(stderr, "Usage: %s <threads>\n", argv[0]);
-		exit(1);
-	}
-
+int main(int argc, char *argv[]) {
 	pthread_t thread_id;
-
 	struct arg_struct args;
-
 	args.arg1 = 0;
 	args.arg2 = 1;
 	args.arg3 = 2;
 	args.arg4 = 3;
-
-for(int i = 0; i < atoi(argv[1]); i++) { // spawn number of threads based on argv[1]
 	pthread_create(&thread_id, NULL, &function, &args);
-
-	sleep(1);
-
-	args.arg1++;
-	args.arg2++;
-	args.arg3++;
-	args.arg4++;
-}
-
 	return 0;
 }
