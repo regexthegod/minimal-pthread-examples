213e9f9c0aec3ab0755d3656f0edbd690584a467
Reggie Ecks <56512282+regexthegod@users.noreply.github.com>
2019-10-13 22:03:21 -0400

    Update join.c

--- join.c
+++ join.c
@@ -1,30 +1,14 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
 #include <pthread.h>
 
-void* function() { // function that threads will be calling
-	printf("Thread spawned\n");
-	sleep(10); // give the function something to do
-	
+pthread_t thread;
+
+void* function() {
+	while(1){}
 	return NULL;
 }
 
-int main(int argc, char *argv[]){
-
-	if(argc < 2){
-		fprintf(stderr, "Usage: %s <threads>\n", argv[0]);
-		exit(1);
-	}
-
-	pthread_t thread_id;
-
-	for(int i = 0; i < atoi(argv[1]); i++) { // spawn number of threads based on argv[1]
-	pthread_create(&thread_id, NULL, &function, NULL);
-	}
-
-	pthread_join(thread_id, NULL); // join last thread then return and exit program cleanly after it completes
-
+int main(int argc, char* argv[]) {
+	pthread_create(&thread, NULL, &function, NULL);
+	pthread_join(thread, NULL);
 	return 0;
 }
