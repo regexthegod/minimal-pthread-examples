a01bb827be05f9167a907bb439863cbec7049068
Reggie Ecks <56512282+regexthegod@users.noreply.github.com>
2019-10-13 22:14:04 -0400

    Update timer.c

--- timer.c
+++ timer.c
@@ -5,31 +5,27 @@
 #include <pthread.h>
 
 void* function() {
-    printf("Function thread spawned\n");
-    while(1){}
-    return NULL;
+	printf("Function thread spawned\n");
+	while(1){}
+	return NULL;
 }
 
 void* timer() {
-    printf("Timer thread spawned. Waiting 10 seconds then exiting\n\n");
-    int counter = 0;
-    while(1) {
-        sleep(1);
-        counter++;
-        if(counter >= 10) 
-            exit(0);
-    }
-    return NULL;
+	printf("Timer thread spawned. Waiting 10 seconds then exiting\n\n");
+	int counter = 0;
+	while(1) {
+		sleep(1);
+		counter++;
+		if(counter >= 10) 
+			exit(0);
+	}
+return NULL;
 }
 
 int main(int argc, char *argv[]) {
-
-pthread_t thread;
-
-pthread_create(&thread, NULL, &function, NULL);
-pthread_create(&thread, NULL, &timer, NULL);
-    
-pthread_join(thread, NULL);
-
-return 0;
+	pthread_t thread;
+	pthread_create(&thread, NULL, &function, NULL);
+	pthread_create(&thread, NULL, &timer, NULL);
+	pthread_join(thread, NULL);
+	return 0;
 }
